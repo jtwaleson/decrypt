1c983799d2b09331613fe061c3a8310df85b8ff0
Jouke Thiemo Waleson <jt.waleson@gmail.com>
2012-08-07 23:12:26 +0200

    More strong characters, scroll along with piped text, always restore normal terminal settings

--- decrypt.py
+++ decrypt.py
@@ -7,38 +7,53 @@ import random
 import string
 
 lines = []
+chance = 0.1
+confirmed_per_line = []
 
-for line in fileinput.input():
-    lines.append(line)
-fileinput.close()
 
 screen = curses.initscr()
 curses.noecho()
 curses.curs_set(0)
 screen.keypad(1)
-confirmed_per_line = [[] for line in lines]
-chance = 0.01
-while True:
+
+def iterate(increase = False):
     still_random = 0
-    chance += 0.01
+    global chance, confirmed_per_line, lines
+    if increase:
+        chance += 0.01
     screen.erase()
-    (x,y) = screen.getmaxyx()
-    for line_num in range(min(len(lines), y-1)):
+    (y,x) = screen.getmaxyx()
+    final_line = len(lines)
+    if final_line > y:
+        first_line = final_line - y
+    else:
+        first_line = 0
+    for line_num in range(first_line, final_line):
         line = lines[line_num]
-        for i in [i  for i in range(len(line)) if i not in confirmed_per_line[line_num]]:
+        for i in [i  for i in range(min(x, len(line))) if i not in confirmed_per_line[line_num]]:
             still_random += 1
             if random.random() < chance:
                 confirmed_per_line[line_num].append(i)
-        random_line = ''.join(random.choice(string.ascii_uppercase + string.digits) if x not in confirmed_per_line[line_num] else line[x] for x in range(len(line)))
+        random_line = ''.join(random.choice(string.punctuation) if col not in confirmed_per_line[line_num] else line[col] for col in range(min(len(line), x)))
         try:
-            screen.addstr(line_num, 0, random_line)
+            screen.addstr(line_num - first_line, 0, random_line)
         except Exception:
             pass
 
     screen.refresh()
     time.sleep(0.1)
-    if still_random < 1:
-        break
-time.sleep(2)
-curses.endwin()
+    return still_random > 0
 
+try:
+    for line in fileinput.input():
+        confirmed_per_line.append([])
+        lines.append(line.rstrip())
+        iterate()
+    fileinput.close()
+    while iterate(True):
+        pass
+    time.sleep(2)
+finally:
+    curses.endwin()
+for line in lines:
+    print line
